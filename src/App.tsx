00290e1425c8f09c70e90e218e6df558ea73af76
vibhan_1234 <vibha.nimbalkar@seosaph.com>
2023-04-07 14:53:07 +0530

    create heading functionality

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className='app'>
+        <span className='heading'>Taskify</span>
+      
+    </div>
+  )
+}
+
+export default App
